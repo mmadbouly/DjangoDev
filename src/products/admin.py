582a4cf828f52967ae1f40d218a67582d6808902
Moustafa Madbouly <moustafa.213321@gmail.com>
2019-04-24 07:09:45 +0200

    changing the app

--- src/products/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-# 3ashan te2dar tshofha fel admin page bta3t django
-
-# da esmo relative import 3ashan el models.py f nafs el level ma3 product.py
-from .models import Product
-
-admin.site.register(Product)
